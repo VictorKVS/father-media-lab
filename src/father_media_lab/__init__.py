16733af956d8f8791d21c1733ad5830a62053180
Виктор <92217904+VictorKVS@users.noreply.github.com>
2026-08-16 14:52:47 +0300

    Add minimal Python package

---
+++ src/father_media_lab/__init__.py
@@ -0,0 +1,8 @@
+"""FATHER Media Lab public research package."""
+
+__version__ = "0.1.0"
+
+PUBLIC_REPOSITORY = True
+MODEL_WEIGHTS_ALLOWED_IN_GIT = False
+REMOTE_GENERATION_ENABLED = False
+VIDEO_GENERATION_PROVEN = False
